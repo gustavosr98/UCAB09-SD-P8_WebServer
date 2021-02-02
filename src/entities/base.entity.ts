54ddcd42702db35b54c17f79cdedc55190b67959
Gustavo Sanchez <gustavosr98@gmail.com>
2021-02-01 23:49:53 -0400

    02/DATABASE: ISO 8601

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -1,4 +1,5 @@
 import { Column, PrimaryGeneratedColumn, CreateDateColumn, DeleteDateColumn } from 'typeorm';
+import { Transform } from 'class-transformer';
 
 export enum Status {
   ENABLED = 'enabled',
@@ -17,8 +18,10 @@ export class BaseEntity {
   status: Status;
 
   @CreateDateColumn()
+  @Transform(({ value }) => new Date(value).toISOString())
   created_date: string;
 
   @DeleteDateColumn()
+  @Transform(({ value }) => new Date(value).toISOString())
   deleted_date?: string;
 }
