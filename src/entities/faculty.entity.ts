ebe3c1c349a50c37848cef46072d4efd5ef49461
Yeisson Venencia <yvenencia.saurith@gmail.com>
2021-02-07 08:00:05 -0400

    04/testing: swagger a los servicios de facultades

--- src/entities/faculty.entity.ts
+++ src/entities/faculty.entity.ts
@@ -2,6 +2,7 @@ import { Entity, OneToMany } from 'typeorm';
 
 import { Entity as MyEntity, School } from '@/entities';
 
+
 @Entity()
 export class Faculty extends MyEntity {
   @OneToMany((type) => School, (school) => school.faculty)
